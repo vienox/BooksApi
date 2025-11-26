81ae431966380237e36e0f438549b1846c2641ac
vienox <niewiem3201@gmail.com>
2025-11-26 16:12:08 +0100

    swagger

--- obj/Debug/net8.0/LibraryApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/LibraryApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+964b405657091dd3adb4d07fd86c0d427bcbb222")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d5cf0c7d8c8c4458898a86e725c1f9fb172cd86")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
